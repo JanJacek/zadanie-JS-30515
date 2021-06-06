9bf296d32e014f84cce617a468b9c14c8479d1a0
Jan Jacek <bobkiewicz.jacek@gmail.com>
2021-06-07 00:26:22 +0200

    zadanie wykonane

---
+++ main.js
@@ -0,0 +1,8 @@
+let number = 10;
+if(number<12){
+    console.log('liczba mniejsza od 12');
+}else if(number=12){
+    console.log('liczba równa 12');
+}else{
+    console.log('liczba więkrza od 12');
+}
